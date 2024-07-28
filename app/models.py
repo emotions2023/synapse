5cfd017e0762383231aa45ac56d1b41a0425843f
t-komori <komoridance1999@gmail.com>
2024-07-28 17:45:03 +0900

    models > users > is_active add

--- app/models.py
+++ app/models.py
@@ -7,7 +7,10 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-    # updated_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    
+    @property
+    def is_active(self):
+        return True
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
