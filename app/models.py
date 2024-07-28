1760beff8859723ce253e67189a54432762b8bdf
t-komori <komoridance1999@gmail.com>
2024-07-28 17:29:29 +0900

    none updated_at colmun

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-    updated_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    # updated_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
